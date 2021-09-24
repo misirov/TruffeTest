f5609cc61a4bb90b40f934dbd335e661bceae8a4
P.M <you@example.com>
2021-09-25 01:56:44 +0200

    Truffle test first commit

---
+++ contracts/hello.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.8.0;
+
+
+contract Hello{
+
+    function hello() public pure returns(string memory){
+        return "I salute you commrade";
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+npm install truffle -g
+
+truffle init
+
+truffle compile
+
+truffle develop
+
+var instance = await Hello.deployed()
+Hello.hello() //call the contract
